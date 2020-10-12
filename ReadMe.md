535add8e553f584cbbe92e33af22a517e1497b2e
tangwei <tangwei@yingzi.com>
2020-10-12 17:05:07 +0800

    init

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+如何打包？
+1. gradle先clean，再build
+2. 在项目目录下路径，genCloud\build\idea-sandbox\plugins，可以找到同名项目文件夹
+3. 右键打包成zip文件
+4. 导入idea，通过硬盘安装
+
+
+
+
+
---
+++ src/main/java/com/scoful/actions/MyTranslateAction.java
@@ -0,0 +1,99 @@
+package com.scoful.actions;
+
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.LocalDateTimeUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.StrUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.ui.JBColor;
+import com.scoful.utils.LoggerUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: tangwei
+ * @date: 2020/10/10 15:07
+ */
+public class MyTranslateAction extends AnAction {
+    private long latestClickTime;
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        if (!isFastClick(1000)) {
+            LoggerUtil.init(getClass().getSimpleName(), LoggerUtil.DEBUG);
+            //获取当前编辑器对象
+            Editor editor = e.getData(CommonDataKeys.EDITOR);
+            //获取选择的数据模型
+            if (editor != null) {
+                SelectionModel selectionModel = editor.getSelectionModel();
+                //获取当前选择的文本
+                String selectedText = selectionModel.getSelectedText();
+                if (StrUtil.isNotBlank(selectedText)) {
+                    if (NumberUtil.isNumber(selectedText)) {
+                        // 时间戳处理
+                        if (selectedText.length() == 13) {
+                            // 时间戳转换为LocalDateTime
+                            LocalDateTime localDateTime = LocalDateTimeUtil.of(Long.parseLong(selectedText));
+                            String format = LocalDateTimeUtil.format(localDateTime, DatePattern.NORM_DATETIME_PATTERN);
+                            balloonNotice(format, editor);
+                        } else {
+                            balloonNotice(selectedText, editor);
+                        }
+                    } else {
+                        balloonNotice(getTransLatesResult(selectedText), editor);
+                    }
+                } else {
+                    balloonNotice("请至少选中一点内容!", editor);
+                }
+            }
+        }
+    }
+
+    private void balloonNotice(String selectedText, Editor editor) {
+        JBPopupFactory factory = JBPopupFactory.getInstance();
+        factory.createHtmlTextBalloonBuilder(selectedText, null, new JBColor(new Color(186, 238, 186), new Color(73, 117, 73)), null)
+               .setFadeoutTime(5000)
+               .createBalloon()
+               .show(factory.guessBestPopupLocation(editor), Balloon.Position.below);
+    }
+
+    private String getTransLatesResult(String txt) {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            ResponseEntity<Map> forEntity = restTemplate.getForEntity("https://fanyi.youdao.com/openapi.do?keyfrom=lulua-net&key=620584095&type=data&doctype=json&version=1.1&q={txt}", Map.class, txt);
+            HttpStatus statusCode = forEntity.getStatusCode();
+            if (statusCode.is2xxSuccessful()) {
+                List data = (List) forEntity.getBody().get("translation");
+                Object result = data.get(0);
+                LoggerUtil.info(forEntity.getBody().toString());
+                return result.toString();
+            }
+        } catch (Exception e) {
+            LoggerUtil.error(e.toString());
+            return "--ERROR,may be lost network--!";
+        }
+        return "--FAILED--!";
+    }
+
+    private boolean isFastClick(long timeMillis) {
+        long time = System.currentTimeMillis();
+        long timeD = time - latestClickTime;
+        if (0 < timeD && timeD < timeMillis) {
+            return true;
+        }
+        latestClickTime = time;
+        return false;
+    }
+}
